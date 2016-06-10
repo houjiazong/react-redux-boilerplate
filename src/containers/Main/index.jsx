0d8401e51cfe58e28b0c8e406138c670e172e53c
houjiazong <houjiazong@gmail.com>
2016-06-10 22:42:45 +0800

    add static

--- src/containers/Main/index.jsx
+++ src/containers/Main/index.jsx
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
+import logo from '../../static/MIcon.png';
 class Main extends Component {
   render() {
     return (
-      <h1>React Redux Boilerplate &copy; 2016 <a href='https://github.com/houjiazong'>houjiazong</a></h1>
+      <h1>
+        <img src={logo} width='50' /> React Redux Boilerplate &copy; 2016 <a href='https://github.com/houjiazong'>houjiazong</a>
+      </h1>
     );
   }
 }
