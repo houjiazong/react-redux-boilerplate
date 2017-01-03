db4ae17951226cbe8d6d9f7a4fa3cf50dc92a98a
houjiazong <houjiazong@gmail.com>
2017-01-03 11:18:54 +0800

    fix extract

--- build/webpack.production.config.js
+++ build/webpack.production.config.js
@@ -42,12 +42,12 @@ module.exports = {
       {
         test: /\.scss$/,
         include: path.resolve(__dirname, '../src/styles'),
-        loader: 'style!css!postcss!sass'
+        loader: ExtractTextPlugin.extract('style', 'css!postcss!sass')
       },
       {
         test: /\.scss$/,
         exclude: path.resolve(__dirname, '../src/styles'),
-        loader: 'style!css?modules&importLoaders=1&localIdentName=[local]_[hash:base64:8]!postcss!sass'
+        loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[local]_[hash:base64:8]!postcss!sass')
       }
     ]
   },
