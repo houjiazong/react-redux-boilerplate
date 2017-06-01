a57fdd4376125538c271e2341ec97864e92fe5a0
houjiazong <houjiazong@gmail.com>
2017-06-01 18:38:16 +0800

    fix server port

--- bin/start.js
+++ bin/start.js
@@ -3,6 +3,6 @@ const debug = require('debug')('app:bin:start')
 const project = require('../config/project.config')
 
 debug('Starting server...')
-require('../server/main').listen(3000, () => {
+require('../server/main').listen(project.server_port, () => {
   debug(`==> 🌎  Server is now running at http://localhost:${project.server_port}.`)
 })
