eb678f17a7ef2122ea24a5b31d7ff5e9eba4666d
houjiazong <houjiazong@gmail.com>
2017-08-15 15:24:20 +0800

    Update webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -78,7 +78,10 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       __DEV__,
-      __PROD__
+      __PROD__,
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
     }),
     new HtmlWebpackPlugin({
       template: project.paths.client('index.html'),
